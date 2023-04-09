c2179b9921ea32a4d518dc11292839a3ce82cfdd
DevanshShukla <devansh1352000@gmail.com>
2023-04-09 23:23:41 +0530

    add a parametrized constructor

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -17,6 +17,10 @@ public class EmployeeController {
 
 	private EmployeeService employeeService;
 
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
 	// add mapping for "/list"
 
 	@GetMapping("/list")
