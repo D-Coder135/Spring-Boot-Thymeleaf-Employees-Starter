e2f4b3bd1a5ee9755796463a8df617c1553afc90
DevanshShukla <devansh1352000@gmail.com>
2023-04-11 20:10:25 +0530

    add a public method saveEmployee() of type String

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -44,6 +45,10 @@ public class EmployeeController {
 		theModel.addAttribute("employee", theEmployee);
 
 		return "employees/employee-form";
+
+	}
+
+	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
 
 	}
 
